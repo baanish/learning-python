41aafaaeb08cb151917b1bd0d8eb5fb4b06e3198
abhirud <abhirud@ncsu.edu>
2021-05-16 15:52:04 +0530

    Added comments and updated the code to work a bit better

--- improvedCreateJSON/improvedCreateJSON.py
+++ improvedCreateJSON/improvedCreateJSON.py
@@ -3,12 +3,14 @@ import csv
 csvReader = csv.reader(open("improvedCreatorInput.csv"))
 jsonFile = open("improvedCreatorJSON.json", "w")
 jsonFields = []
-readFirstLine = False
 for inputData in csvReader:
-    if(readFirstLine) :
+    #use the first line to get the names of the fields
+    if(csvReader.line_num != 1):
+        #check if first line of value
         if(csvReader.line_num == 2):
             jsonFile.write("{")
         else:
+        #add a comma and new line if not first line
             jsonFile.write(" ,\n{")
         i = 0
         for value in inputData:
@@ -19,8 +21,7 @@ for inputData in csvReader:
             i = i + 1
     else :
         for field in inputData:
+            #store the names in a list
             jsonFields.append(field)
-            readFirstLine = True
         jsonFile.write("{ \"data\": [\n")
 jsonFile.write("\n]\n}")
-
